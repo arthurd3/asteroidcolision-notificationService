31928f2ef710f269e4bbe400b4ef11af4ca4dc6a
arthurd3 <arthur_camposl@yahoo.com>
2025-08-27 10:04:19 -0300

    feat: create route to nasa api

--- asteroidcolision/src/main/java/com/arthur/asteroidcolision/client/NasaClient.java
+++ asteroidcolision/src/main/java/com/arthur/asteroidcolision/client/NasaClient.java
@@ -1,14 +1,37 @@
 package com.arthur.asteroidcolision.client;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.time.LocalDate;
 
 @Service
 public class NasaClient {
 
+    @Value("${nasa.neo.api.url}")
+    private String nasaNeoApiUrl;
+
+    @Value("${mynasa.api.key}")
+    private String nasaApiKey;
+
     public List<Asteroid> getNeoAsteroid(final LocalDate fromDate , final LocalDate toDate){
         final RestTemplate restTemplate = new RestTemplate();
+
+        final NasaNeoResponse nasaNeoResponse =
+                restTemplate.getForObject(getUrl(fromDate , toDate), NasaNeoResponse.class);
+    }
+
+    public String getUrl(final LocalDate fromDate, final LocalDate toDate){
+        String apiUrl = UriComponentsBuilder.fromUriString(nasaNeoApiUrl)
+                .queryParam("start_date", fromDate)
+                .queryParam("end_date", toDate)
+                .queryParam("api_key", this.nasaApiKey)
+                .toUriString();
+
+        return apiUrl;
+
+
     }
 }
