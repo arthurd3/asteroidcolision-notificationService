93b8b0053de2831b2d2456099aff766b53ff5dc3
arthurd3 <arthur_camposl@yahoo.com>
2025-08-27 10:25:31 -0300

    feat: create classes for asteroids

--- asteroidcolision/src/main/java/com/arthur/asteroidcolision/client/NasaClient.java
+++ asteroidcolision/src/main/java/com/arthur/asteroidcolision/client/NasaClient.java
@@ -1,11 +1,15 @@
 package com.arthur.asteroidcolision.client;
 
+import com.arthur.asteroidcolision.dto.Asteroid;
+import com.arthur.asteroidcolision.dto.NasaNeoResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class NasaClient {
@@ -16,11 +20,20 @@ public class NasaClient {
     @Value("${mynasa.api.key}")
     private String nasaApiKey;
 
-    public List<Asteroid> getNeoAsteroid(final LocalDate fromDate , final LocalDate toDate){
+    public List<Asteroid> getNeoAsteroids(final LocalDate fromDate , final LocalDate toDate){
         final RestTemplate restTemplate = new RestTemplate();
 
         final NasaNeoResponse nasaNeoResponse =
                 restTemplate.getForObject(getUrl(fromDate , toDate), NasaNeoResponse.class);
+
+
+        List<Asteroid> asteroidList = new ArrayList<>();
+
+        if(nasaNeoResponse != null){
+            asteroidList.addAll(nasaNeoResponse.getNearEarthObjects().values().stream().flatMap(List::stream).toList());
+        }
+
+        return asteroidList;
     }
 
     public String getUrl(final LocalDate fromDate, final LocalDate toDate){
--- asteroidcolision/src/main/java/com/arthur/asteroidcolision/service/AsteroidAlertingService.java
+++ asteroidcolision/src/main/java/com/arthur/asteroidcolision/service/AsteroidAlertingService.java
@@ -1,16 +1,23 @@
 package com.arthur.asteroidcolision.service;
 
+import com.arthur.asteroidcolision.client.NasaClient;
+import com.arthur.asteroidcolision.dto.Asteroid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @Slf4j
 public class AsteroidAlertingService {
 
     private final NasaClient nasaClient;
+
+    public AsteroidAlertingService(NasaClient nasaClient) {
+        this.nasaClient = nasaClient;
+    }
 
     public void alert(){
         log.info("Alert of Asteroid Alerting");
@@ -19,6 +26,6 @@ public class AsteroidAlertingService {
         final LocalDate toDate = LocalDate.now().plusDays(7);
 
         log.info("Getting asteroid alerts from {} to {}", fromDate, toDate);
-        nasaClient.getNeoAsteroids(fromDate, toDate);
+        final List<Asteroid> asteroidList = nasaClient.getNeoAsteroids(fromDate, toDate);
     }
 }
